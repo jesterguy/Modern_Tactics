eefcbad284df850a41a8938bf47197010afb8f9b
jesterguy <jesterguy1324@wowway.com>
2013-05-26 14:54:35 -0500

    forgot these, my bad

---
+++ Modern_Tactics/Form2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+using SharpGL;
+using SharpGL.Enumerations;
+using System.Configuration;
+
+namespace Modern_Tactics
+{
+   public partial class Form2 : Form
+   {
+      public Form2()
+      {
+         InitializeComponent();
+         this.openGLControl1.DrawFPS = true;
+      }
+       
+      private void openGLControl1_OpenGLDraw(object sender, PaintEventArgs e)
+      {
+         OpenGL gl = this.openGLControl1.OpenGL;
+
+         gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+         gl.LoadIdentity();
+
+         int MapHeight = 100;
+         int MapWidth = 100;
+
+        float[] darkColor = {1.2f, 0.2f, 0.2f};
+	    float[] lightColor = {1.25f, 0.25f, 0.25f};
+	    for(int h=0; h < MapHeight; h++ ){
+		    if( h % 10 == 0){
+			    gl.LineWidth(2.0f);
+			    gl.Color(lightColor);
+		    }else{
+			    gl.LineWidth(1.0f);
+			    gl.Color(darkColor);
+		    }
+		    gl.Begin(OpenGL.GL_LINES);							// Start Drawing Verticle Cell Borders
+			    gl.Vertex4i(0, h*32,0,2);// Left Side Of Horizontal Line
+                gl.Vertex4i(MapWidth * 32, h * 32, 0, 2);// Right Side Of Horizontal Line
+		    gl.End();
+	    }
+	    for(int v=0; v < MapWidth; v++ ){
+		    if( v % 10 == 0){
+			    gl.LineWidth(2.0f);
+			    gl.Color(lightColor);
+		    }else{
+			    gl.LineWidth(1.0f);
+			    gl.Color(darkColor);
+		    }
+		    gl.Begin(OpenGL.GL_LINES);							// Start Drawing Verticle Cell Borders
+                gl.Vertex4i(v * 32, 0, 0, 2);// Left Side Of Horizontal Line
+                gl.Vertex4i(v * 32, MapHeight * 32, 0, 2);// Right Side Of Horizontal Line
+		    gl.End();	
+	    }
+
+        gl.Flush();
+      }
+
+      private void openGLControl1_OpenGLInitialized(object sender, EventArgs e)
+      {
+          SharpGL.OpenGL gl = this.openGLControl1.OpenGL;
+
+          int WinWidth = Int32.Parse(ConfigurationManager.AppSettings["WINDOW_WIDTH"]);
+          int WinHeight = Int32.Parse(ConfigurationManager.AppSettings["WINDOW_HEIGHT"]);
+
+          gl.Viewport(0, 0, WinWidth, WinHeight);
+
+          gl.ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+          gl.MatrixMode(OpenGL.GL_PROJECTION);
+          gl.LoadIdentity();
+
+          gl.Ortho(0, WinWidth, WinHeight, 0, -1, 1);
+
+          gl.MatrixMode(OpenGL.GL_MODELVIEW);
+
+      }
+
+      private void openGLControl1_Resized(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+      {
+      }
+   }
+}
---
+++ Modern_Tactics/Form3.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Modern_Tactics
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openGLControl1 = new SharpGL.OpenGLControl();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl1
+            // 
+            this.openGLControl1.BitDepth = 32;
+            this.openGLControl1.DrawFPS = false;
+            this.openGLControl1.FrameRate = 28;
+            this.openGLControl1.Location = new System.Drawing.Point(12, 12);
+            this.openGLControl1.Name = "openGLControl1";
+            this.openGLControl1.RenderContextType = SharpGL.RenderContextType.FBO;
+            this.openGLControl1.Size = new System.Drawing.Size(579, 455);
+            this.openGLControl1.TabIndex = 0;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 479);
+            this.Controls.Add(this.openGLControl1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl1;
+    }
+}
