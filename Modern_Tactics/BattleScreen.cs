584857e73951525c4056fe0b38e4dd23e40aaeb9
jesterguy <jesterguy1324@wowway.com>
2013-05-27 13:50:02 -0500

    moved grid drawing into its own function

--- Modern_Tactics/BattleScreen.cs
+++ Modern_Tactics/BattleScreen.cs
@@ -40,68 +40,7 @@ namespace Modern_Tactics
        
       private void openGLControl1_OpenGLDraw(object sender, PaintEventArgs e)
       {
-         OpenGL gl = this.openGLControl1.OpenGL;
-
-         //find a way to run only on resize
-         InitializeOpenGL();
-
-         gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
-         gl.LoadIdentity();
-
-         int MapHeight = 100;
-         int MapWidth = 100;
-
-         float[] darkColor = { 0.2f, 0.2f, 0.2f };
-         float[] lightColor = { 0.25f, 0.25f, 0.25f };
-         for (int h = 0; h < MapHeight; h++)
-         {
-             if (h % 10 == 0)
-             {
-                 gl.LineWidth(2.0f);
-                 gl.Color(lightColor);
-             }
-             else
-             {
-                 gl.LineWidth(1.0f);
-                 gl.Color(darkColor);
-             }
-             gl.Begin(OpenGL.GL_LINES);							// Start Drawing Verticle Cell Borders
-             gl.Vertex(0, h * 32, 0);// Left Side Of Horizontal Line
-             gl.Vertex(MapWidth * 32, h * 32, 0);// Right Side Of Horizontal Line
-             gl.End();
-         }
-         for (int v = 0; v < MapWidth; v++)
-         {
-             if (v % 10 == 0)
-             {
-                 gl.LineWidth(2.0f);
-                 gl.Color(lightColor);
-             }
-             else
-             {
-                 gl.LineWidth(1.0f);
-                 gl.Color(darkColor);
-             }
-             gl.Begin(OpenGL.GL_LINES);							// Start Drawing Verticle Cell Borders
-             gl.Vertex(v * 32, 0, 0);// Left Side Of Horizontal Line
-             gl.Vertex(v * 32, MapHeight * 32, 0);// Right Side Of Horizontal Line
-             gl.End();
-         }
-
-          // temp thing to show tile map is building right
-         gl.Begin(OpenGL.GL_QUADS);
-         for (int y = 0; y < battleMap.mapHeight; y++)
-         {
-             for (int x = 0; x < battleMap.mapWidth; x++)
-             {
-                 gl.Vertex(battleMap.tileMap[y][x].x, battleMap.tileMap[y][x].y, 0);
-                 gl.Vertex(battleMap.tileMap[y][x].x + 32, battleMap.tileMap[y][x].y, 0);
-                 gl.Vertex(battleMap.tileMap[y][x].x, battleMap.tileMap[y][x].y + 32, 0);
-                 gl.Vertex(battleMap.tileMap[y][x].x + 32, battleMap.tileMap[y][x].y + 32, 0);
-             }
-         }
-         gl.End();
-
+          DrawGrid();
       }
 
       private void openGLControl1_OpenGLInitialized(object sender, EventArgs e)
@@ -112,6 +51,72 @@ namespace Modern_Tactics
       private void openGLControl1_Resized(object sender, EventArgs e)
       {
           InitializeOpenGL();
+      }
+
+      private void DrawGrid()
+      {
+          OpenGL gl = this.openGLControl1.OpenGL;
+
+          //find a way to run only on resize
+          InitializeOpenGL();
+
+          gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+          gl.LoadIdentity();
+
+          int MapHeight = 100;
+          int MapWidth = 100;
+
+          float[] darkColor = { 0.2f, 0.2f, 0.2f };
+          float[] lightColor = { 0.25f, 0.25f, 0.25f };
+          for (int h = 0; h < MapHeight; h++)
+          {
+              if (h % 10 == 0)
+              {
+                  gl.LineWidth(2.0f);
+                  gl.Color(lightColor);
+              }
+              else
+              {
+                  gl.LineWidth(1.0f);
+                  gl.Color(darkColor);
+              }
+              gl.Begin(OpenGL.GL_LINES);							// Start Drawing Verticle Cell Borders
+              gl.Vertex(0, h * 32, 0);// Left Side Of Horizontal Line
+              gl.Vertex(MapWidth * 32, h * 32, 0);// Right Side Of Horizontal Line
+              gl.End();
+          }
+          for (int v = 0; v < MapWidth; v++)
+          {
+              if (v % 10 == 0)
+              {
+                  gl.LineWidth(2.0f);
+                  gl.Color(lightColor);
+              }
+              else
+              {
+                  gl.LineWidth(1.0f);
+                  gl.Color(darkColor);
+              }
+              gl.Begin(OpenGL.GL_LINES);							// Start Drawing Verticle Cell Borders
+              gl.Vertex(v * 32, 0, 0);// Left Side Of Horizontal Line
+              gl.Vertex(v * 32, MapHeight * 32, 0);// Right Side Of Horizontal Line
+              gl.End();
+          }
+
+          // temp thing to show tile map is building right
+          gl.Begin(OpenGL.GL_QUADS);
+          for (int y = 0; y < battleMap.mapHeight; y++)
+          {
+              for (int x = 0; x < battleMap.mapWidth; x++)
+              {
+                  gl.Vertex(battleMap.tileMap[y][x].x, battleMap.tileMap[y][x].y, 0);
+                  gl.Vertex(battleMap.tileMap[y][x].x + 32, battleMap.tileMap[y][x].y, 0);
+                  gl.Vertex(battleMap.tileMap[y][x].x, battleMap.tileMap[y][x].y + 32, 0);
+                  gl.Vertex(battleMap.tileMap[y][x].x + 32, battleMap.tileMap[y][x].y + 32, 0);
+              }
+          }
+          gl.End();
+
       }
    }
 }
